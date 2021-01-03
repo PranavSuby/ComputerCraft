2f84fe705412d4fcf8f1ae928aabf37d395c08ca
Pranav Suby <subypranav@gmail.com>
2021-01-03 08:53:04 -0500

    Original Files

---
+++ hud.lua
@@ -0,0 +1,48 @@
+require "lib"
+
+local modules = peripheral.wrap(back)
+local canvas = modules.canvas()
+local modem =  peripheral.wrap(left)
+local reactorTextBox = canvas.addGroup({ 0, 0 })
+local armorTextBox = canvas.addGroup({0, 30})
+
+if modem.isOpen(2) ~= true then
+  modem.open(2)
+end
+
+while true do
+  local event, p1, p2, p3, p4, p5, p6 = os.pullEvent()
+
+  if event == "modem_message" then
+    local message = p4
+
+    if message == "Reactor Warning!!!" then
+      local text = reactorTextBox.addText({ 5, 5 }, message)
+      text.setScale(3)
+      text.setColor(255, 0, 0, 255)
+
+    elseif message == "Reactor Good" then
+      reactorTextBox.clear()
+
+    elseif message == "Repair Living Armor Now" then
+      local text = armorTextBox.addText({5, 5}, message)
+      text.setScale(1.75)
+      text.setColor(255, 0, 0, 255)
+
+    elseif message == "Armor Good"  then
+      armorTextBox.clear()
+    end
+
+  elseif event=="key" then
+    local key = p1
+
+    if key == keys.q then
+      reactorTextBox.clear()
+      armorTextBox.clear()
+      break
+    end
+
+  end
+
+
+end
