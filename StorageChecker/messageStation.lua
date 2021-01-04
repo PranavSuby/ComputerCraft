28d2ef07241509eeaa1b97a75e3adf578c00a76e
Pranav Suby <subypranav@gmail.com>
2021-01-04 16:30:51 -0500

    Fixed bug messageStation

--- StorageChecker/messageStation.lua
+++ StorageChecker/messageStation.lua
@@ -19,9 +19,9 @@ end
 --TODO When checkStock.lua sends a message changing the color, I have to send a message erasing the color from the hud and remaking it, maybe put that in setItemFalse(). I also need to send a message when everythign is good
 
 for i=1, #drawerTable do
-  messageSentTable[drawerTable[i].getItemMeta(2).name.."-Red")] = False
-  messageSentTable[drawerTable[i].getItemMeta(2).name.."-Yellow")] = False
-  messageSentTable[drawerTable[i].getItemMeta(2).name.."-Green")] = False
+  messageSentTable[drawerTable[i].getItemMeta(2).name.."-Red"] = False
+  messageSentTable[drawerTable[i].getItemMeta(2).name.."-Yellow"] = False
+  messageSentTable[drawerTable[i].getItemMeta(2).name.."-Green"] = False
 end
 
 while true do
