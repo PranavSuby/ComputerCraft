cb2c2854f77c870cd53007ba23a49766000f9800
Pranav Suby <subypranav@gmail.com>
2021-01-05 06:00:02 -0500

    deleted storageLib

--- StorageChecker/messageStation.lua
+++ StorageChecker/messageStation.lua
@@ -3,6 +3,14 @@ require "storageLib"
 
 local modem = peripheral.wrap(left)
 
+local oakLeaves = "storagedrawers:basicdrawers_1_0"
+
+drawerTable = {
+  peripheral.wrap(oakLeaves)
+}
+
+drawerTableReverse = reverseIndex(drawerTable)
+
 openModem(1, modem)
 openModem(3, modem)
 
