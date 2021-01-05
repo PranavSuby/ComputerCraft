f9a0ea45729108d327df149dc6b8cb1dc873d811
Pranav Suby <subypranav@gmail.com>
2021-01-05 06:11:42 -0500

    Storage lib is back

--- StorageChecker/checkStock.lua
+++ StorageChecker/checkStock.lua
@@ -1,14 +1,7 @@
 require "lib"
+require "storageLib"
 
 local modem = peripheral.wrap(left)
-
-local oakLeaves = "storagedrawers:basicdrawers_1_0"
-
-drawerTable = {
-  peripheral.wrap(oakLeaves)
-}
-
-drawerTableReverse = reverseIndex(drawerTable)
 
 while true do
 
---
+++ StorageChecker/storageLib.lua
@@ -0,0 +1,7 @@
+require "lib"
+
+drawerTable = {
+  peripheral.wrap("storagedrawers:basicdrawers_1_0") --OakLeaves
+}
+
+drawerTableReverse = reverseIndex(drawerTable)
